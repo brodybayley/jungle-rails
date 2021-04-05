273e9216c8ab8174b43249c2c1b632f7a90fb7de
Brody Bayley <brodybayley@gmail.com>
2021-04-05 23:16:08 +0000

    Add test cases for product

--- spec/models/product_spec_spec.rb
+++ spec/models/product_spec_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe ProductSpec, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Product, type: :model do
+
+  describe 'Validations' do
+
+    it 'creates a product' do
+      @category = Category.create(name: "plants")
+      @product = Product.create(name: "shamrock", price: 2500, quantity: 10, category: @category)
+    end
+
+    it 'contains a name' do
+      @category = Category.create(name: "plants")
+      @product = Product.create(name: nil, price: 2500, quantity: 10, category: @category)
+    end
+
+    it 'contains a price' do
+      @category = Category.create(name: "plants")
+      @product = Product.create(name: "shamrock", price: nil, quantity: 10, category: @category)
+    end
+
+    it 'contains a quantity' do
+      @category = Category.create(name: "plants")
+      @product = Product.create(name: "shamrock", price: 2500, quantity: nil, category: @category)
+    end
+
+    it 'contains a category' do
+      @category = Category.create(name: "plants")
+      @product = Product.create(name: "shamrock", price: nil, quantity: 10, category: nil)
+    end
+
+  end
 end
