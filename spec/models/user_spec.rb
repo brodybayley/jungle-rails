f881bb89157745b590f041a77308cba403720a57
Brody Bayley <brodybayley@gmail.com>
2021-04-06 00:04:59 +0000

    Add validation cases for fields in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,29 @@ RSpec.describe User, type: :model do
     it 'creates a user' do 
       @user = User.create(first_name: 'Shaungel', last_name: 'Robin', email: 'shon25@gmail.com', password: "hello1", password_confirmation: "hello1")
     end
+
+    it 'must contain a password' do 
+      @user = User.create(first_name: 'Shaungel', last_name: 'Robin', email: 'shon25@gmail.com', password: nil, password_confirmation: "hello1")
+    end
+
+    it 'must contain a password confirmation' do 
+      @user = User.create(first_name: 'Shaungel', last_name: 'Robin', email: 'shon25@gmail.com', password: "hello1", password_confirmation: nil)
+    end
+
+    it 'must contain a matching password and password confirmation' do 
+      @user = User.create(first_name: 'Shaungel', last_name: 'Robin', email: 'shon25@gmail.com', password: "hello1", password_confirmation: "hola1")
+    end
+
+    it 'must contain an email' do 
+      @user = User.create(first_name: 'Shaungel', last_name: 'Robin', email: nil, password: "hello1", password_confirmation: "hello1")
+    end
+
+    it 'must contain a first name' do 
+      @user = User.create(first_name: nil, last_name: 'Robin', email: 'shon25@gmail.com', password: "hello1", password_confirmation: "hello1")
+    end
+
+    it 'must contain a last name' do 
+      @user = User.create(first_name: 'Shaungel', last_name: nil, email: 'shon25@gmail.com', password: "hello1", password_confirmation: "hello1")
+    end
   end
 end
